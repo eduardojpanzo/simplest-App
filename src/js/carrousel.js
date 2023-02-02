2203d35549eedfce7bd22a6315b956b8d783e079
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2023-02-02 22:41:47 +0100

    feat: carrousel action

---
+++ src/js/carrousel.js
@@ -0,0 +1,39 @@
+const slider = document.querySelector("#slider");
+let sliderSection = document.querySelectorAll(".slider__section");
+let sliderSectionLast = sliderSection[sliderSection.length - 1];
+
+const btnLeft = document.querySelector("#btn_left");
+const btnRight = document.querySelector("#btn_right");
+
+slider.insertAdjacentElement("afterbegin", sliderSectionLast);
+
+function moveToRight() {
+  let sliderSectionFirst = document.querySelectorAll(".slider__section")[0];
+  slider.style.marginLeft = "-200%";
+  slider.style.transition = "all 0.5s";
+
+  setTimeout(() => {
+    slider.style.transition = "none";
+    slider.insertAdjacentElement("beforeend", sliderSectionFirst);
+    slider.style.marginLeft = "-100%";
+  }, 500);
+}
+function moveToLeft() {
+  let sliderSection = document.querySelectorAll(".slider__section");
+  let sliderSectionLast = sliderSection[sliderSection.length - 1];
+  slider.style.marginLeft = "0";
+  slider.style.transition = "all 0.5s";
+
+  setTimeout(() => {
+    slider.style.transition = "none";
+    slider.insertAdjacentElement("afterbegin", sliderSectionLast);
+    slider.style.marginLeft = "-100%";
+  }, 500);
+}
+
+btnRight.addEventListener("click", moveToRight);
+btnLeft.addEventListener("click", moveToLeft);
+
+setInterval(() => {
+  moveToRight();
+}, 3500);

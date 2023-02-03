7aaad0ddd5c64f7833d65a6bc3e452622e940e44
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2023-02-03 17:33:18 +0100

    feat: Datafetch and consumer

--- src/js/filterBar.js
+++ src/js/filterBar.js
@@ -1,6 +1,9 @@
+const storeitem = document.getElementById("product-list");
+
+getData();
+
 const search = () => {
   const searchbox = document.getElementById("search-item").value.toUpperCase();
-  const storeitem = document.getElementById("product-list");
   const product = document.querySelectorAll(".product");
   const pname = document.getElementsByTagName("h2");
 
@@ -18,3 +21,22 @@ const search = () => {
     }
   }
 };
+
+async function getData() {
+  const response = await fetch(
+    "https://jsonplaceholder.typicode.com/albums/1/photos"
+  );
+  const data = await response.json();
+
+  storeitem.innerHTML = "";
+
+  data.map((data) => {
+    storeitem.innerHTML += `<div class="product">
+    <img src=${data.url} alt=${data.title} />
+    <div class="p-details">
+      <h2>${data.title}</h2>
+      <h3>$2${data.id}</h3>
+    </div>
+  </div>`;
+  });
+}

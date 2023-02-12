cb5a5dcf2d0106a3e128d92b55308221585cc776
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2023-02-12 22:02:37 +0100

    chore: more javascript

--- src/js/carrousel.js
+++ src/js/carrousel.js
@@ -1,7 +1,27 @@
 const slider = document.querySelector("#slider");
+const sliders = [
+  {
+    url: "https://s32907.pcdn.co/wp-content/uploads/2019/09/escrit%C3%B3rios-pequenos-21.png",
+    name: "imagem1",
+  },
+  {
+    url: "https://s32907.pcdn.co/wp-content/uploads/2019/09/Escrit%C3%B3rios-pequenos-1.jpg",
+    name: "imagem2",
+  },
+  {
+    url: "https://s32907.pcdn.co/wp-content/uploads/2019/09/escrit%C3%B3rios-pequenos-15.jpg",
+    name: "imagem3",
+  },
+  {
+    url: "https://s32907.pcdn.co/wp-content/uploads/2019/09/escrit%C3%B3rios-pequenos-3.jpg",
+    name: "imagem4",
+  },
+];
+
+boot();
+
 let sliderSection = document.querySelectorAll(".slider__section");
 let sliderSectionLast = sliderSection[sliderSection.length - 1];
-
 const btnLeft = document.querySelector("#btn_left");
 const btnRight = document.querySelector("#btn_right");
 
@@ -37,3 +57,18 @@ btnLeft.addEventListener("click", moveToLeft);
 setInterval(() => {
   moveToRight();
 }, 3500);
+
+function boot() {
+  slider.innerHTML = "";
+
+  sliders.forEach(({ name, url }) => {
+    slider.innerHTML += `
+    <div class="slider__section">
+      <img
+        src=${url}
+        alt=${name}
+        class="slider__img"
+      />
+    </div>`;
+  });
+}

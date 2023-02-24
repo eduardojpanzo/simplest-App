637654404180a1f667abb78aa7e6268ea22f5e09
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2023-02-24 13:25:37 +0100

    feat: dinamic elements in header

--- src/js/dash.js
+++ src/js/dash.js
@@ -1,21 +1,58 @@
 const body = document.querySelector("body");
+const subMenu = document.querySelector("#subMenu");
+
 const sidebar = body.querySelector(".sidebar");
 const toggle = body.querySelector(".toggle");
 const searchBtn = body.querySelector(".search-box");
 const modeText = body.querySelector(".mode-text");
 const modeSwitch = body.querySelector(".toggle-switch");
 
-toggle.addEventListener("click", () => {
-  sidebar.classList.toggle("close");
-});
+setSubMenuLinks();
 
-modeSwitch.addEventListener("click", () => {
-  body.classList.toggle("dark");
+// toggle.addEventListener("click", () => {
+//   sidebar.classList.toggle("close");
+// });
 
-  if (body.classList.contains("dark")) {
-    modeText.innerHTML = "Light Mode";
-    return;
-  }
+// modeSwitch.addEventListener("click", () => {
+//   body.classList.toggle("dark");
 
-  modeText.innerHTML = "Dark Mode";
-});
+//   if (body.classList.contains("dark")) {
+//     modeText.innerHTML = "Light Mode";
+//     return;
+//   }
+
+//   modeText.innerHTML = "Dark Mode";
+// });
+
+function toggleShowSubMenu() {
+  subMenu.classList.toggle("open-menu");
+}
+
+function setSubMenuLinks() {
+  subMenu.querySelector(".user-info").innerHTML = `
+  <img src="${userData.img}" alt="${userData.name}" />
+  <h3>${userData.name}</h3>
+  `;
+
+  subMenu.querySelector(".links").innerHTML = "";
+  subMenuLinks.forEach((item, i) => {
+    subMenu.querySelector(".links").innerHTML += `
+  <a href="${item.path}" class="sub-menu-link">
+    <img src="${item.icon}" alt="${item.value}" />
+    <p>${item.value}</p>
+    <span>></span>
+  </a>
+  `;
+  });
+}
+
+function setuserInitialDate() {
+  document.querySelector("header .imagotipo").innerHTML = `
+  <img src="${logo.src}" alt="${logo.alt}" />
+
+  <div class="text">
+    <span class="name">${userData.company.name}</span> <br />
+    <span class="profession">${userData.company.office}</span>
+  </div>
+  `;
+}
---
+++ src/mock/dash.js
@@ -0,0 +1,18 @@
+const logo = {
+  src: "",
+  alt: "",
+};
+
+const userData = {
+  name: "John Doe",
+  img: "",
+  company: {
+    name: "Dline-code",
+    office: "Dev Front-End",
+  },
+};
+
+const subMenuLinks = [
+  { value: "Edit Profile", path: "/dash/edit.html", icon: "" },
+  { value: "Out", path: "index.html", icon: "" },
+];

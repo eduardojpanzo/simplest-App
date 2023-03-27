4fe2b4dc13fdfbe1637497153b1f1192395cfc0a
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2023-03-28 00:49:56 +0100

    feat: update todo task

--- src/js/todo.js
+++ src/js/todo.js
@@ -1,6 +1,6 @@
 let todos = todoData ? todoData : [];
 
-const CreateTodoModalFrame = `
+const createTodoModalFrame = `
 <form class="todo-preview" onsubmit="handlecreateTodo(event)">
     <div class="control-form">
         <label for="title">Title: </label>
@@ -30,42 +30,6 @@ const CreateTodoModalFrame = `
 </form>
 `;
 
-const PreviewTodoModalFrame = `
-<form class="todo-preview" onsubmit="handleUpdateTodo(event)">
-    <input type="hidden" id="id" name="id">
-    <div class="control-form">
-    <input type="text" name="title" id="title" />
-    </div>
-
-    <div class="todo-resume">
-    <select name="priority" id="priority">
-        <option value="high">High</option>
-        <option value="medium">Medium</option>
-        <option value="low">Low</option>
-    </select>
-    <select name="status" id="status">
-        <option value="done">Done</option>
-        <option value="process">Process</option>
-        <option value="toDo">ToDo</option>
-    </select>
-    </div>
-
-    <div class="control-form about">
-    <label for="desc">About TODO</label>
-    <textarea name="desc" id="desc">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, voluptatum!</textarea
-    >
-    </div>
-
-    <div class="control-form">
-    <label>Create At</label>
-    <input type="date" name="date" id="date" />
-    </div>
-
-    <button class="btn">+</button>
-</form>
-`;
-
 const btnSwithTheme = document.querySelector(".btn.switch-theme");
 const btnToggleModal = document.querySelectorAll(".btn.toggle-modal");
 const modalOverlay = document.querySelector(".modal-overlay");
@@ -86,31 +50,19 @@ function toggleTheme(e) {
 function handleToggleShowCreateTodoModal() {
   toggleShowModal()
     ? (modalOverlay.querySelector(".modal-content").innerHTML =
-        CreateTodoModalFrame)
+        createTodoModalFrame)
+    : (modalOverlay.querySelector(".modal-content").innerHTML = "");
+}
+
+function handleToggleShowUpdateTodoModal(data) {
+  toggleShowModal()
+    ? (modalOverlay.querySelector(".modal-content").innerHTML =
+        mountFormUpdate(data))
     : (modalOverlay.querySelector(".modal-content").innerHTML = "");
 }
 
 function toggleShowModal() {
   return modalOverlay.classList.toggle("modal-show");
-}
-
-function initTodoApp() {
-  btnSwithTheme.innerHTML = svgDark;
-  btnToggleModal[0].innerHTML = svgAdd;
-
-  contentItems.innerHTML = ``;
-  todos.map((todo) => {
-    contentItems.innerHTML += `
-    <div class="todo-item">
-    <div class="title">${todo.title}</div>
-    <div class="todo-resume">
-      <span class="priority">${todo.priority}</span>
-      <span class="staus">${todo.status}</span>
-    </div>
-    <p class="create-at">${todo.date}</p>
-  </div>
-    `;
-  });
 }
 
 function handlecreateTodo(e) {
@@ -130,6 +82,94 @@ function handlecreateTodo(e) {
   localStorage.setItem("todoData", JSON.stringify(todos));
   initTodoApp();
   toggleShowModal();
+}
+
+function handleUpdateTodo(e) {
+  e.preventDefault();
+
+  const newData = {
+    id: e.target.id,
+    title: e.target.title.value,
+    priority: e.target.priority.value,
+    status: e.target.status.value,
+    desc: e.target.desc.value,
+    date: e.target.date.value,
+  };
+
+  todos.map((todo) => {
+    if (todo.id === newData.id) {
+      return newData;
+    }
+
+    return todo;
+  });
+
+  console.log(todos);
+
+  // localStorage.setItem("todoData", JSON.stringify(todos));
+  // initTodoApp();
+  // toggleShowModal();
+}
+
+function mountTodos() {
+  contentItems.innerHTML = ``;
+  todos.map((todo) => {
+    contentItems.innerHTML += `
+    <div class="todo-item" onclick="handleToggleShowUpdateTodoModal(${todo})">
+      <div class="title">${todo.title}</div>
+      <div class="todo-resume">
+        <span class="priority">${todo.priority}</span>
+        <span class="staus">${todo.status}</span>
+      </div>
+      <p class="create-at">${todo.date}</p>
+    </div>
+    `;
+  });
+}
+
+function mountFormUpdate(data) {
+  const updateFormFrame = `
+  <form class="todo-preview" onsubmit="handleUpdateTodo(event)">
+      <input type="hidden" id="id" name="id" value="${data.id}">
+      <div class="control-form">
+        <input type="text" name="title" id="title" value="${data.title}"/>
+      </div>
+
+      <div class="todo-resume">
+      <select name="priority" id="priority">
+          <option value="high">High</option>
+          <option value="medium">Medium</option>
+          <option value="low">Low</option>
+      </select>
+      <select name="status" id="status">
+          <option value="done">Done</option>
+          <option value="process">Process</option>
+          <option value="toDo">ToDo</option>
+      </select>
+      </div>
+
+      <div class="control-form about">
+      <label for="desc">About TODO</label>
+      <textarea name="desc" id="desc"> ${data.des}</textarea>
+      </div>
+
+      <div class="control-form">
+      <label>Create At</label>
+      <input type="date" name="date" id="date" value="${data.date}" />
+      </div>
+
+      <button class="btn">+</button>
+  </form>
+  `;
+
+  return updateFormFrame;
+}
+
+function initTodoApp() {
+  btnSwithTheme.innerHTML = svgDark;
+  btnToggleModal[0].innerHTML = svgAdd;
+
+  mountTodos();
 }
 
 initTodoApp();

d6a1ee8883a1f08d2467b041ac088b4945f0256d
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2023-05-25 22:57:03 +0100

    feat: genarate box and items - kanban

---
+++ src/data/kanban.js
@@ -0,0 +1,31 @@
+let kabanBoxesData = [
+  { id: 1, title: "Todo" },
+  { id: 2, title: "Progress" },
+];
+
+let kabanItemsData = [
+  {
+    id: 1,
+    boxId: 1,
+    title: "Title1",
+    content: "lorem same things sakt hiudg hasb",
+    date: "20/05/2023",
+    level: "medium",
+  },
+  {
+    id: 1,
+    boxId: 2,
+    title: "Title3",
+    content: "lorem same things sakt hiudg hasb",
+    date: "20/05/2023",
+    level: "high",
+  },
+  {
+    id: 1,
+    boxId: 1,
+    title: "Title4",
+    content: "lorem same things sakt hiudg hasb",
+    date: "20/05/2023",
+    level: "low",
+  },
+];
---
+++ src/js/kanban.js
@@ -0,0 +1,64 @@
+const content = document.querySelector("main .content");
+
+function mountTheBoxes() {
+  const boxes = kabanBoxesData
+    .map(
+      (box) => `
+    <div class="box" data-boxid="${box.id}">
+        <div class="box-header">
+        <span>${box.title}</span>
+
+        <i class="fas fa-ellipsis-v"></i>
+        </div>
+
+        <div class="box-content">
+        </div>
+
+        <div class="box-add">
+        <i class="fas fa-plus-circle"></i>
+        Add Item
+        </div>
+    </div>
+    `
+    )
+    .join("");
+
+  content.innerHTML = boxes;
+}
+
+function mountTheBoxesItems() {
+  const boxes = content.querySelectorAll(".box");
+
+  boxes.forEach((box) => {
+    const boxContent = box.querySelector(".box-content");
+    const boxId = box.getAttribute("data-boxid");
+
+    boxContent.innerHTML = "";
+
+    kabanItemsData.forEach((item) => {
+      if (item.boxId == boxId) {
+        boxContent.innerHTML += `
+        <div class="item ${item.level}" data-key="${item.id}" draggable="true">
+            <div class="item-header">
+            <span class="item-date">${item.date}</span>
+
+            <div class="item-level">${item.level}</div>
+            </div>
+            <div class="item-content">
+            <h3>${item.title}</h3>
+            <p>
+                ${item.content}
+            </p>
+            </div>
+        </div>`;
+      }
+    });
+  });
+}
+
+function init() {
+  mountTheBoxes();
+  mountTheBoxesItems();
+}
+
+init();
